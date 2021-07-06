86f7f3a2cc02aa1051fd1f354501c3cbe829cdc0
jlalojl <82975975+jlalojl@users.noreply.github.com>
2021-07-06 13:27:51 -0500

    Update Database.php

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="blogelin";
+		$this->user="root";$this->pass="abc123";$this->host="localhost";$this->ddbb="blogelin";
 	}
 
 	function connect(){
